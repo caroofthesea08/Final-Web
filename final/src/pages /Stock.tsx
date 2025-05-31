60acdd9b8ba53895c715eb1ec61a6c23f59ecf06
Nicolas <nicocuabrango1230@gmail.com>
2025-05-30 22:35:27 -0500

    Avanzando

---
+++ final/src/pages /Stock.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Stock: React.FC = () => {
+  return (
+    <div>
+      <h1>Gestión de Inventario</h1>
+      <p>Aquí podrás ver, agregar, editar y eliminar productos de maquillaje.</p>
+    </div>
+  );
+};
+
+export default Stock;
